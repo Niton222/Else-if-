d96aa9bd5a5404671353344b1f10b10dfc87cecc
Niton222 <105493288+Niton222@users.noreply.github.com>
2022-09-22 18:52:39 +0200

    Add files via upload

---
+++ GHJ.js
@@ -0,0 +1,72 @@
+ var prompt = require('prompt-sync')();
+ /*var x = prompt("Write number:")
+if (x<0){
+    console.log ("Negative number:"+x)
+}
+if (x>0){
+    console.log ("Positive number:"+x)
+}
+*/
+var y = prompt ("Write Letter or Number:");{
+    if (isNaN){
+        console.log("Letter:"+y);
+    }
+    if (y<0){
+        console.log ("Negative number:"+y)
+    }
+    if (y>0){
+        console.log ("Positive number:"+y)
+    }
+}
+
+
+
+function Vokall(){
+    var A = A;
+    var E = E;
+    var I = I;
+    var O = O;
+    var U = U;
+    var Y = Y;
+    var A = A;
+    var Ä = Ä;
+    var Ö = Ö;
+}
+function Number(){
+    var ty = 1;
+    var tr = 2; 
+    var te = 3;
+    var ti = 4;
+    var tl = 5;
+    var tn = 6;
+    var tb = 7;
+    var tg = 8;
+    var th = 9;
+    var ts = 0;
+}
+var b = prompt ("Write Vokal or konstant:");
+var Product = Kex;
+if('Number'){
+    Product.call(this, Kex);
+     }
+     
+if ( b = 'Vokal' ){
+        console.log("Vokal"+b+"Uber")
+    }
+function Kex(){
+    var b = prompt ("Write Vokal or konstant:");
+    if('Number'){
+        Product.call(this, Kex);
+        }
+        if ( b = 'Vokall' ){
+            console.log("Vokal"+b+"Uber")
+        }
+}
+ 
+
+
+
+
+
+
+
